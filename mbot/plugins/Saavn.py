b3e2ae81bafab547c228083243d6c2346e4d61f5
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-08-26 05:34:19 +0530

    Update Saavn.py

--- mbot/plugins/Saavn.py
+++ mbot/plugins/Saavn.py
@@ -4,41 +4,21 @@ import urllib.parse
 import aiohttp
 import asyncio
 
-def format_copyable_text(text):
-    return f"`{text}`"
-
-async def get_similar_results(query):
-    # Simulate a search request for similar songs based on the user's query
-    similar_results = []
-
-    similar_search_url = f"https://saavn.me/search/songs?query={query}&page=1&limit=5"
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(similar_search_url) as response:
-                similar_search_results = await response.json()
-        # Extract the song names or other relevant data from the results
-        for result in similar_search_results.get('data', {}).get('results', []):
-            similar_results.append(result.get('name', ''))
-    except Exception as e:
-        print(f"Error fetching similar results: {str(e)}")
-
-    return similar_results
 
 @Client.on_message(filters.text & (filters.private | filters.group))
 async def search_and_send_song(client, message):
     query = urllib.parse.quote(message.text)
-
-    # Simulate the "searching" status
+    if query.startswith("/"):
+        return
+        
     searching_msg = await message.reply("Searching...")
-
     url = f"https://saavn.me/search/songs?query={query}&page=1&limit=1"
-
+    
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 r = await response.json()
     except Exception as e:
-        await message.reply(f"Error: {str(e)}")
         await searching_msg.delete()
         return
 
@@ -86,15 +66,6 @@ async def search_and_send_song(client, message):
 
     except (KeyError, IndexError):
         await message.reply("No results found.")
-        # Get similar results and create an inline keyboard
-        similar_results = await get_similar_results(query)
-        if similar_results:
-            inline_keyboard = []
-            for result in similar_results:
-                formatted_text = format_copyable_text(result)
-                inline_keyboard.append([types.InlineKeyboardButton(formatted_text, callback_data=result)])
-            inline_keyboard_markup = types.InlineKeyboardMarkup(inline_keyboard)
-            await message.reply("Did you mean one of these?", reply_markup=inline_keyboard_markup)
-
+        
     finally:
         await searching_msg.delete()
