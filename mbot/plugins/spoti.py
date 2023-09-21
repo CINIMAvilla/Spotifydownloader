f5c7c9e0dc19cfd79ada76b1a68fe92896e82d1c
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2023-09-21 08:33:21 +0530

    Update spoti.py

--- mbot/plugins/spoti.py
+++ mbot/plugins/spoti.py
@@ -1,35 +1,37 @@
-from pyrogram import Client, filters
-from spotipy import Spotify
+import re
+import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from bs4 import BeautifulSoup
-import requests
+from pyrogram import Client, filters
 
-sp = Spotify(auth_manager=SpotifyClientCredentials(client_id="857ba4e2288e43bda42a43cf76fd84d3",
-                                                   client_secret="a25b7d3b6b03437aa82870ef06dbd65d"))
+YOUR_CLIENT_ID = "857ba4e2288e43bda42a43cf76fd84d3"
+YOUR_CLIENT_SECRET = "a25b7d3b6b03437aa82870ef06dbd65d"
 
-@Client.on_message(filters.regex(r'https://open\.spotify\.com/track/([a-zA-Z0-9]+)'))
-def get_spotify_info(client, update):
-    spotify_track_url = update.text
-    track_id = spotify_track_url.split("/")[-1]
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=YOUR_CLIENT_ID,
+                                                           client_secret=YOUR_CLIENT_SECRET))
+                                                
+async def send_track(chat_id, track_info):
+    track_name = track_info['name']
+    artists = ', '.join(artist['name'] for artist in track_info['artists'])
+    audio_url = track_info['preview_url']
+    caption = f"🎵 Track: {track_name} by {artists}"
+    await client.send_audio(chat_id=chat_id, audio=audio_url, caption=caption)
 
-    try:
+async def send_playlist_tracks(chat_id, playlist_tracks):
+    for track_info in playlist_tracks:
+        await send_track(chat_id, track_info['track'])
+
+@Client.on_message(filters.regex(r'https?://open\.spotify\.com/(track|playlist)/[a-zA-Z0-9]+'))
+async def handle_spotify_link(client, message):
+    chat_id = message.chat.id
+    link = message.matches[0].group(0)
+
+    if 'track' in link:
+        # Single track link
+        track_id = link.split('/')[-1]
         track_info = sp.track(track_id)
-        track_name = track_info['name']
-        artists = [artist['name'] for artist in track_info['artists']]
-
-        saavn_url = get_saavn_link(track_name, artists)
-        send_music_to_user(client, update.chat.id, saavn_url)
-    except Exception as e:
-        print(f"Error: {e}")
-      
-def get_saavn_link(track_name, artists):
-    saavn_search_url = f"https://www.saavn.me/search/{track_name}-{artists[0]}/"
-    response = requests.get(saavn_search_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    saavn_url = soup.find("a", class_="btn btn-primary")['href']
-    return saavn_url
-
-def send_music_to_user(client, chat_id, saavn_url):
-    response = requests.get(saavn_url)
-    music_content = response.content
-    client.send_audio(chat_id, audio=music_content, title="Music", performer="Artist")
+        await send_track(chat_id, track_info)
+    elif 'playlist' in link:
+        # Playlist link
+        playlist_id = link.split('/')[-1]
+        playlist_info = sp.playlist_tracks(playlist_id)
+        await send_playlist_tracks(chat_id, playlist_info['items'])
